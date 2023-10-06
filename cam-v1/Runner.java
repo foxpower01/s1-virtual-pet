f44f4a0a5c16f3bcc80386044a3d7dc32610719b
Riley Newfield <rileyin24@ausdk12.org>
2023-10-06 15:04:24 -0700

    10/6/23

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,7 +1,39 @@
-public class Runner {
-    public static void main(String[] args) {
-        VirtualPet p = new VirtualPet();
+import java.util.*;
+import java.util.Timer;
 
+import javax.swing.*;
+
+public class Runner {
+    public Runner(){
+        VirtualPet p = new VirtualPet();
         p.exercise();
+
+        Timer timer = new Timer();
+            ((Object) timer).schedule(new TimerTask() {
+                public void run() {
+                            //call whatever method
+                }
+            }, 0, 5000);
+
+
+    }
+
+    public void ageIncrease(VirtualPet p){
+        
+    }
+
+    public String getResponse(String q){
+        String s = (String)JOptionPane.showInputDialog(new JFrame(), q, "Get a response", JOptionPane.PLAIN_MESSAGE);
+        return s;
+    }
+
+    private void pause(long milliseconds){
+        try{
+            Thread.sleep(milliseconds);
+        }catch(Exception e){}
+    }
+
+    public static void main(String[] args) {
+        new Runner();
     }
 }
--- cam-v1/VirtualPet.java
+++ cam-v1/VirtualPet.java
@@ -3,17 +3,30 @@
  * @author Cam
  * @author ?
  */
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class VirtualPet {
     
     VirtualPetFace face;
     private int hunger = 0;   // how hungry the pet is.
     private int tiredness = 0;
+    private int age = 0;
     
     // constructor
     public VirtualPet() {
         face = new VirtualPetFace();
         face.setImage("normal");
         face.setMessage("Hello.");
+    }
+
+    //getter and setter for age
+
+    private void pause(long milliseconds){
+        try{
+            Thread.sleep(milliseconds);
+        }catch(Exception e){}
     }
     
     public void feed() {
@@ -22,8 +35,40 @@ public class VirtualPet {
         } else {
             hunger = 0;
         }
+        if ((int)(Math.random() * 10) == 9){
+            face.setMessage("Your pet is sick!");
+            face.setImage("sick");
+        }
         face.setMessage("Yum, thanks");
         face.setImage("normal");
+    }
+
+    public void handleSick(){
+        String s = (String)JOptionPane.showInputDialog(new JFrame(), "Do you want to take your pet to the vet?", "Get a response", JOptionPane.PLAIN_MESSAGE);
+        if (s.indexOf("yes") > -1 || s.indexOf("Yes") > -1){
+            goToVet();
+        }else{
+            face.setImage("sleep");
+            pause(3000);
+            if ((int)(Math.random() * 3) == 3){
+                face.setMessage("Your pet got better on its own!");
+                face.setImage("normal");
+            }else{
+                face.setMessage("Your pet doesn't feel so good...");
+                pause(2000);
+                dead();
+            }
+        }
+    }
+
+    private void goToVet(){
+
+    }
+
+    public void dead(){
+        face.setImage("dead");
+        pause(2000);
+        face.setImage("skeleon");
     }
     
     public void exercise() {
